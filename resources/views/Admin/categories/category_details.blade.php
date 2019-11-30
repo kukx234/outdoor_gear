784dad461dfc06adfecbe806c989dd50ce2b0b92
tina234 <martina.dejanovic94@gmail.com>
2019-11-30 13:02:36 +0100

    uredeni detalji kategorije

--- resources/views/Admin/categories/category_details.blade.php
+++ resources/views/Admin/categories/category_details.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.adminnav')
 
 @section('content')
-<div>
+<div class="category-list-form">
     <h1>{{ $category->title }}</h1>
 
     @if (!$sub_categories->isEmpty())
-        <div class="sub-categories">
+        <div class="sub-categories list-title">
             <div> 
-                Podkategorije
-                <span>{{ count($sub_categories) }}</span>
+                <h3>Podkategorije</h3> 
+                <div class="span-count">
+                    <span>{{ count($sub_categories) }}</span>
+                </div>
             </div>
             <ul>
                 @foreach ($sub_categories as $sub_category)
@@ -22,8 +24,13 @@
     @endif
 
     @if (!$products->isEmpty())
-    <span>Produkti ({{ count($products) }}) </span>
-        <div class="products">
+        <div class="products list-title">
+            <div>
+                <h3>Produkti</h3>
+                <div class="span-count">
+                    <span>{{ count($products) }} </span>
+                </div>
+            </div>
             <ul>
                 @foreach ($products as $product)
                     <li>
@@ -34,10 +41,11 @@
             </ul>
         </div>
     @endif
-
-    <a class="makeup onhover" href="{{ route('category_edit', $category->id) }}">Uredi</a>
-    <button class="delete onhover" onclick="popupAlert('categoriesdelete',{{ $category->id }})">Obriši</button>
-    <button onclick="goBack()">Nazad</button>
+    <div class="list-buttons detail-btns">
+        <a class="makeup onhover" href="{{ route('category_edit', $category->id) }}">Uredi</a>
+        <!--<button class="delete onhover" onclick="popupAlert('categoriesdelete',{{ $category->id }})">Obriši</button>-->
+        <button onclick="goBack()"><i class="fas fa-arrow-circle-left"></i></button>
+    </div>
 </div>
 
 @endsection
